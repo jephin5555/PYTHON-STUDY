c549cc95674b850aff08b92ff8ac7a1bf541bcef
jephin <jjose2267@conestogac.on.ca>
2021-03-06 17:59:05 -0500

    dictionary collections

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+men={"jeph":10,"rekhin":12,"sambu":22}
+men["jeph"]=11
+print(men)
+
+men=({"name":"Raju","age":"kp"},
+    {"name":"Ru","age":"jkp"},
+    {"name":"ju","age":"koop"}
+)
+#dictionary 1st row printing
+print(men[0])
